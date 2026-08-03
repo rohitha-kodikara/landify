d1d5b578ef8ef303025ba540446f3f03e08d0641
S.K.ROHITHA <samarawickramarohitha392@gmail.com>
2026-08-03 15:04:10 +0530

    removed unnecesary styles.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,49 +19,51 @@ const Hero = () => {
           </Button>
           <Button className="bg-gray-800 text-white h-10">Free preview</Button>
         </div>
-        {/*parent div*/}
-        <div>
-          <div className="flex flex-row items-center">
-            <img
-              src="/p1.jpg"
-              className="relative z-40 rounded-full w-9 h-9 border-2 border-white object-cover"
-              alt="avatar 1"
-            />
-            <img
-              src="/p2.jpg"
-              className="relative -ml-3 z-30 rounded-full w-9 h-9 border-2 border-pink-300 object-cover"
-              alt="avatar 2"
-            />
-            <img
-              src="/p1.jpg"
-              className="relative -ml-3 z-20 rounded-full w-9 h-9 border-2 border-purple-400 object-cover"
-              alt="avatar 3"
-            />
-            <img
-              src="/p2.jpg"
-              className="relative -ml-3 z-10 rounded-full w-9 h-9 border-2 border-sky-300 object-cover"
-              alt="avatar 4"
-            />
-            <div className="flex flex-col gap-1 px-3 items-start">
-              <span className="text-white text-xs leading-relaxed">
-                Trusted by 1M+ customers
-              </span>
-              <div className="flex flex-row gap-1 items-center ">
-                <Star className="text-yellow-400 fill-yellow-400 h-4 -ml-0.5" />
-                <Star className="text-yellow-400 fill-yellow-400 h-4" />
-                <Star className="text-yellow-400 fill-yellow-400 h-4" />
-                <Star className="text-yellow-400 fill-yellow-400 h-4" />
-                <Star className="text-yellow-400 fill-yellow-400 h-4" />{" "}
-                <span className="text-white text-[10px] leading-none">
-                  4.2/5 (45k reviews)
+
+        <div className="p-5">
+          <div className="flex flex-col items-center gap-3">
+            <div className="flex flex-row items-center">
+              <img
+                src="/p1.jpg"
+                className="relative z-40 rounded-full w-9 h-9 border-2 border-white object-cover"
+                alt="avatar 1"
+              />
+              <img
+                src="/p2.jpg"
+                className="relative -ml-3 z-30 rounded-full w-9 h-9 border-2 border-pink-300 object-cover"
+                alt="avatar 2"
+              />
+              <img
+                src="/p1.jpg"
+                className="relative -ml-3 z-20 rounded-full w-9 h-9 border-2 border-purple-400 object-cover"
+                alt="avatar 3"
+              />
+              <img
+                src="/p2.jpg"
+                className="relative -ml-3 z-10 rounded-full w-9 h-9 border-2 border-sky-300 object-cover"
+                alt="avatar 4"
+              />
+              <div className="flex flex-col gap-1 px-3 items-start">
+                <span className="text-white text-xs leading-relaxed">
+                  Trusted by 1M+ customers
                 </span>
+                <div className="flex flex-row gap-1 items-center">
+                  <Star className="text-yellow-400 fill-yellow-400 h-4 -ml-0.5" />
+                  <Star className="text-yellow-400 fill-yellow-400 h-4" />
+                  <Star className="text-yellow-400 fill-yellow-400 h-4" />
+                  <Star className="text-yellow-400 fill-yellow-400 h-4" />
+                  <Star className="text-yellow-400 fill-yellow-400 h-4" />{" "}
+                  <span className="text-white text-[10px] leading-none">
+                    4.2/5 (45k reviews)
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/*parent div*/}
       </div>
     </div>
   );
 };
+
 export default Hero;
